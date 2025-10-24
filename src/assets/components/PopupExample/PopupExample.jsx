ec40843c927f5d580355c70ae78958216e43952b
Mukharbi <muhrik_naruto@mail.ru>
2025-10-24 17:02:25 +0300

    update

--- src/assets/components/PopupExample/PopupExample.jsx
+++ src/assets/components/PopupExample/PopupExample.jsx
@@ -1,25 +1,32 @@
-import { useRef, useState, lazy } from "react";
+import { useState } from "react";
 import styles from "./styles.module.scss";
 
 const PopupExample = () => {
   const [info, setInfo] = useState(false);
+  const [isAnimation, setIsAnimation] = useState(false);
 
-  const openInfo = () => {
+  const handlOpen = () => {
     setInfo(true);
+    setTimeout(() => setIsAnimation(true), 10);
   };
 
-  const closeInfo = () => {
-    setInfo(false);
+  const handClose = () => {
+    setIsAnimation(false);
+    setTimeout(() => setInfo(false), 300);
   };
 
   return (
     <div className={styles.container}>
       <p>Узнать секретный ингредиент секретно-ингредиентного кода:</p>
-      <button type="button" className={styles.button_door} onClick={openInfo}>
+      <button type="button" className={styles.button_door} onClick={handlOpen}>
         ⤴︎
       </button>
       {info && (
-        <div className={styles.wrapper}>
+        <div
+          className={`${styles.wrapper} ${
+            isAnimation ? styles.open : styles.close
+          }`}
+        >
           <div className={styles.container_info}>
             <div className={styles.info}>
               <p>
@@ -29,7 +36,7 @@ const PopupExample = () => {
               <button
                 type="button"
                 className={styles.button_secret}
-                onClick={closeInfo}
+                onClick={handClose}
               >
                 ☹
               </button>
