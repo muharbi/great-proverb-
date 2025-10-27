8f335f651bdde06bd04740af537999991421b3f8
Mukharbi <muhrik_naruto@mail.ru>
2025-10-27 10:00:07 +0300

    update

--- src/assets/components/MisterShu/MisterShu.jsx
+++ src/assets/components/MisterShu/MisterShu.jsx
@@ -3,6 +3,7 @@ import styles from "./styles.module.scss";
 const MisterShu = () => {
   return (
     <div className={styles.container_img}>
+      <p>console.log("Hello script")</p>
       <img src="./mister Shu.png" alt="shu" />
     </div>
   );
